3a5019c7c97fb70bca4d5d77f4854be7c5b407a7
josedev-union <josebarato321@gmail.com>
2023-11-08 20:07:40 +0200

    Add waitapp logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,50 @@ set -e
 #     5. Generate prefix based on the git ref automatically if not presented.
 
 
-compose_ctx_path=${INPUT_CONTEXT:-.caprover}
-app_name_prefix=${INPUT_PREFIX:-pr}
-event_id=$(echo "$GITHUB_REF" | awk -F / '{print $3}')
-
+########################### Global var def
+COMPOSE_CTX_PATH=${INPUT_CONTEXT:-.caprover}
+APP_NAME_PREFIX=${INPUT_PREFIX:-pr}
+EVENT_ID=$(echo "$GITHUB_REF" | awk -F / '{print $3}')
+## caprover cli config vars
 export CAPROVER_URL=$INPUT_SERVER
 export CAPROVER_PASSWORD=${INPUT_PASSWORD:-captain42}
 export CAPROVER_NAME=default
-# caprover login
+## caprover api request vars
+NS="x-namespace: captain"
+CTYPE="Content-Type: application/json"
+# getToken gets the token from Caprover API.
+getToken() {
+  res=$(curl -sSf "$CAPROVER_URL/api/v2/login" -X POST -d '{"password":"'$CAPROVER_PASSWORD'"}' -H "$CTYPE" -H "$NS")
+  token=$(echo "$res"|awk -F'"token":"' '{print $2}'|awk -F'"' '{print $1}'|grep .)
+  return $token
+}
+AUTH="x-captain-auth: ${getToken}"
 
+########################### Function def
+
+# waitApp waits until the app is ready.
+#
+# Arguments:
+#     $1: app name.
+#
+waitApp() {
+  app_name=$1
+  for i in {1..10}; do
+    res=$(curl -sSf "$CAPROVER_URL/api/v2/user/apps/appData/$app_name" -H "$CTYPE" -H "$NS" -H "$AUTH")
+    echo $res|jq '.description' -r && echo "$res"|grep '"status":100,' >/dev/null
+    is_building=$(echo $res|jq '.data.isAppBuilding')
+    echo "App building: $is_building"
+    if [ "$is_building" == "false" ]; then
+      echo "App is ready now!"
+      break
+    else
+      echo "Waiting until ready (try: $i)"
+      sleep 10
+    fi
+  done
+}
+
+# setOutput does action output
 setOutput() {
     echo "${1}=${2}" >> "${GITHUB_OUTPUT}"
 }
@@ -53,12 +88,14 @@ ensureSingleApp() {
   res=$(caprover deploy --appName $app_name -c $app_ctx_path/captain-definition)
   if [ $? -eq 0 ]; then
     set -e
-    echo "[app:$app_alias] successfully deployed! $res";
+    echo "$res";
+    echo "[app:$app_alias] successfully deployed!";
   else
     set -e
     if [[ "$res" == *"not exist"* ]]; then
       echo "[app:$app_alias] create a new app as it doesn't exist!";
       createApp $app_name;
+      waitApp $app_name;
       echo "[app:$app_alias] deployment step!";
       caprover deploy --appName $app_name -c $app_ctx_path/captain-definition;
     else
@@ -86,10 +123,12 @@ preValidate() {
 }
 
 generateAppName() {
-  echo "${app_name_prefix}-${GITHUB_REPOSITORY_ID}-${event_id}-${1}"
+  echo "${APP_NAME_PREFIX}-${GITHUB_REPOSITORY_ID}-${EVENT_ID}-${1}"
 }
 
-for app in $compose_ctx_path/*/; do
+########################### Main
+preValidate
+for app in $COMPOSE_CTX_PATH/*/; do
   echo "Deploying $(basename "$app") app...";
   ensureSingleApp "${app}" "$(basename $app)"
 done
